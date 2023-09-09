2a463cb558146b1e8e9a95ac546bdb79b7b6f9da
Vinson Beduya <vinsonemb.151994@gmail.com>
2023-09-09 20:30:53 +1200

    Added Styled Navigation Items

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,11 +1,48 @@
+"use client";
+
 import styles from "@/styles/components/nav-bar.module.css";
 import Link from "next/link";
+import GroupIcon from "@mui/icons-material/Group";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { usePathname } from "next/navigation";
+
+interface Path {
+  name: string;
+  href: string;
+  icon: () => React.ReactElement<typeof PersonAddIcon | typeof GroupIcon>;
+}
+
+const paths: Array<Path> = [
+  {
+    name: "Save Account",
+    href: "/save-account",
+    icon: () => <PersonAddIcon fontSize="medium" />,
+  },
+  {
+    name: "View Accounts",
+    href: "/",
+    icon: () => <GroupIcon fontSize="medium" />,
+  },
+];
 
 const NavBar = () => {
+  const path = usePathname();
+  const isCurrentPath = (href: string) => href === path;
+
   return (
     <nav className={styles.navBar}>
-      <Link href="/save-account">Save Account</Link>
-      <Link href="/">View Accounts</Link>
+      {paths.map((path, index) => (
+        <Link
+          href={path.href}
+          className={`${styles.navItem} ${
+            isCurrentPath(path.href) && styles.navItemActive
+          }`}
+          key={`${index}-${path.name}`}
+        >
+          {path.icon()}
+          {path.name}
+        </Link>
+      ))}
     </nav>
   );
 };
